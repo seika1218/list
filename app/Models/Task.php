f8350d819e8f254c894b78d37eaff0799f1bce5f
平田聖歌 <seikah124@gmail.com>
2022-10-26 18:28:07 +0900

    インデント

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,9 +11,9 @@ class Task extends Model
      * 状態定義
      */
     const STATUS = [
-        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
-        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
-        3 => [ 'label' => '完了', 'class' => '' ],
+        1 => ['label' => '未着手', 'class' => 'label-danger'],
+        2 => ['label' => '着手中', 'class' => 'label-info'],
+        3 => ['label' => '完了', 'class' => ''],
     ];
 
     /**
